a7ae3702d7591f4ca527ad71300670ed97edda7c
이지민 <dlwlalsdkd@gmail.com>
2024-06-21 15:31:15 +0900

    [test] vercel 호스팅 에러 테스트

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,14 +10,14 @@ export default function NotFound() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          test page
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">당장 돌아가세요</p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
         >
-          Back to homepage
+          제발 돌아가!
         </Link>
       </div>
     </div>
