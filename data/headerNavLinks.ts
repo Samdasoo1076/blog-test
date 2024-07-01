c8f9e4f3c83fd72a2b917457cec7f93eb640a28c
이지민 <dlwlalsdkd@gmail.com>
2024-07-01 09:03:05 +0900

    [test] vercel build test

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: '태그' },
   { href: '/projects', title: '프로젝트' },
   { href: '/about', title: '기타' },
-  { href: '/doje', title: '도제 6기' },
+  { href: '/doje', title: '도제 소개' },
 ]
 
 export default headerNavLinks
