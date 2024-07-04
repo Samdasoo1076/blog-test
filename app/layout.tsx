e8fda5bd726dd85ef26fb7543163bea926b39fec
이지민 <dlwlalsdkd@gmail.com>
2024-07-04 11:27:07 +0900

    [test] 구글 애드 센스 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <meta name="google-site-verification" content="htVmYTqtNq9BrUufzlm8evqFYP8qBc3BiSu9Y75ir74" />
+      <meta name="google-adsense-account" content="ca-pub-5133707720452623"></meta>
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
