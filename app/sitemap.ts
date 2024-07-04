fa858a607430ae6c059a0471fea47ee3dc4f0e7c
이지민 <dlwlalsdkd@gmail.com>
2024-07-04 15:51:14 +0900

    [hotfix] sitemap.ts도 url 맞게 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,10 +12,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+  const routes = ['', 'blog', 'projects', 'tags', 'about', 'doje', 'doje6', 'doje7', 'doje8'].map(
+    (route) => ({
+      url: `${siteUrl}/${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })
+  )
 
   return [...routes, ...blogRoutes]
 }
